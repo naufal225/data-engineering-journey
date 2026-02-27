01a8787b67ef03e04fc7f785cb558ed0088f16f6
naufal225 <naufalmarufashrori225@gmail.com>
2026-02-27 09:12:39 +0700

    insert ke database postgres

--- Minggu 2/Hari 4/src/02_load_to_postgres.py
+++ Minggu 2/Hari 4/src/02_load_to_postgres.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import requests
+import psycopg2
+from psycopg2.extras import execute_values
+import os
 
 def fetch_request(url:str):
     r = requests.get(url=url, timeout=10)
@@ -51,10 +54,56 @@ df_joined["title_length"] = df_joined["title"].astype(str).str.len()
 
 validate(df_joined)
 
-# to list of tuples
+# connect postgres
 
-data = list(df_joined.itertuples(name=None, index=False))
+conn = psycopg2.connect(
+    host=os.getenv("PGHOST", "localhost"),
+    port=os.getenv("PGPORT", "5432"),
+    database=os.getenv("PGDATABASE", "praktek_etl_simpel"),
+    user=os.getenv("PGUSER", "postgres"),
+    password=os.getenv("PGPASSWORD", "nma225")
+)
 
-for i,v in enumerate(data):
-    print(v)
-    # mau lakuin query disini
+conn.autocommit = False
+
+try:
+    with conn.cursor() as cur:
+        cols = ["post_id", "user_id", "user_name", "user_email", "title_length", "body_length"]
+        rows = list(df_joined[cols].itertuples(name=None, index=False))
+        
+        sql = f"""
+        INSERT INTO joined_analytics ({",".join(cols)})
+        VALUES %s
+        ON CONFLICT (post_id) DO UPDATE SET
+            user_id = EXCLUDED.user_id,
+            user_name = EXCLUDED.user_name,
+            user_email = EXCLUDED.user_email,
+            title_length = EXCLUDED.title_length,
+            body_length = EXCLUDED.body_length;
+        """
+        
+        execute_values(cur, sql, rows, page_size=1000)
+        
+        cur.execute("SELECT COUNT(*) FROM joined_analytics;")
+        total = cur.fetchone()[0]
+        
+        cur.execute("""
+                    SELECT user_name, COUNT(*) as post_count
+                    FROM joined_analytics
+                    GROUP BY user_name
+                    ORDER BY post_count DESC, user_name ASC
+                    LIMIT 3;
+                    """)
+        
+        top3 = cur.fetchall()
+        
+    conn.commit()
+    
+    print("Inserted/Upserted rows:", len(df_joined))
+    print("Total:",total)
+    print("Top 3:", top3)
+except Exception as e:
+    conn.rollback()
+    raise
+finally:
+    conn.close()
